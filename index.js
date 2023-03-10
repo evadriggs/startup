ad20a1ad3dd10198f4f74c7fe939d5c768ba99f7
evadriggs <evajasinski@gmail.com>
2023-03-10 15:52:24 -0700

    first draft final push!

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 function login() {
     const nameEl = document.querySelector("#name");
     localStorage.setItem("userName", nameEl.value);
-    const petNameRow = document.querySelector("tr:contains('Pet Name')");
-    petNameRow.querySelector("td:last-child").textContent = nameEl.value;
     window.location.href = "pet.html";
   }
--- pet.js
+++ pet.js
@@ -1,11 +1,25 @@
-// Feeding the pet//
 
+//Variables
+const button = document.getElementById("food-button");
+const result = document.getElementById("result");
+const stages = ['#egg', '#caterpillar', '#pupa', '#moth'];
+var current_stage = 0;
+
+//Setting pet name 
+function setName() {
+    var petName = localStorage.getItem('userName');
+    const petNameRow = document.getElementById("pet-name");
+    petNameRow.textContent = petName;
+}
+// Feeding the pet
 function feedMe() {
+    result.textContent = "I'm hungry!"
+    const meter = document.getElementById("meter")
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         const success = Math.random() < 0.8;
         if (success) {
-          resolve("Yum! Thank you!"); 
+          resolve("Yum! Thank you!");
         } else {
           reject("Ugh I'm full. Just give me a sec"); 
         }
@@ -20,16 +34,16 @@ function feedMe() {
     .catch(error => {
       console.log(error); 
     });
-  
-  
-  const button = document.getElementById("food-button");
-  const result = document.getElementById("result");
-  
+    
   button.addEventListener("click", () => {
     button.textContent = "Eating...";
   
     feedMe()
       .then(message => {
+        meter.setAttribute('value', parseInt(meter.getAttribute('value')) + 25);
+        if (meter.getAttribute('value') >= 100) {
+            evolve()
+          } 
         result.textContent = message;
       })
       .catch(error => {
@@ -40,7 +54,23 @@ function feedMe() {
       });
   });
 
-// Pet Evolution //
+// Pet Evolution 
 function evolve() {
-    
+//change sprite
+  if (current_stage < 3) {
+  var stage = stages[current_stage];
+  var pet = document.querySelector(stage);
+  pet.style.display = 'none';
+  current_stage ++;
+  stage = stages[current_stage];
+  var pet = document.querySelector(stage);
+  pet.style.display = 'flex';
+  }
+//reset hunger
+  meter.setAttribute('value', 0)
+//increase age NOT WORKING!!!!
+  var age = document.getElementById("age");
+  age.innerHTML = parseInt(age.innerHTML) + 1;
 }
+
+setName()
