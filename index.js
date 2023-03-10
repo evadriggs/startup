568dbceea9a8e9982985e672ea771b7a48a21318
evadriggs <evajasinski@gmail.com>
2023-03-10 13:45:51 -0700

    fix login

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function login() {
     const nameEl = document.querySelector("#name");
     localStorage.setItem("userName", nameEl.value);
-    window.location.href = "play.html";
+    const petNameRow = document.querySelector("tr:contains('Pet Name')");
+    petNameRow.querySelector("td:last-child").textContent = nameEl.value;
+    window.location.href = "pet.html";
   }
